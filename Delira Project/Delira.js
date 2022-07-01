6ccbfa0403917b5a46699a81c1d8bb0909efd803
Gabriel Menegatti Reis <gabrielmenegattireis@gmail.com>
2022-07-01 03:56:32 -0300

    JavaScript added

---
+++ Delira Project/Delira.js
@@ -0,0 +1,11 @@
+console.log("Hello World!");
+ function returnEvenValues(array){
+    let evemnums = [];
+    for(let i=0;i< array.length;i++)
+    if(array[i]%2===0){
+        evennums.push(array[i]);
+        else
+        console.log('${array[i]}');
+    }
+ }
+ console.log(evennums);
